634a19571a8a3dced8cc44da44298e1302a60cad
Angel Motta <angel.motta@utec.edu.pe>
2023-06-05 22:23:00 -0500

    feat: endpoint definitions

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/angelmotta/flow-currency-api/internal/exchangestore"
+	"github.com/go-chi/chi/v5"
 	"log"
+	"net/http"
 )
 
 type CurrencyServer struct {
@@ -19,15 +22,51 @@ func NewCurrencyServer() *CurrencyServer {
 }
 
 func main() {
-	log.Println("Hello world")
-
 	// Create a new CurrencyServer
-	cServer := NewCurrencyServer()
-	val, err := cServer.rdb.GetExchange("usd_pen_")
+	currServer := NewCurrencyServer()
+
+	// Retrieve data from DB
+	val, err := currServer.rdb.GetExchange("usd_pen_")
 	if err != nil {
 		log.Fatalf("error getting exchange rate: %v", err)
 	} else if val == "" {
 		log.Println("currency exchange does not exist")
 	}
 	log.Println("currency exchange: ", val)
+
+	// Create a new router
+	r := chi.NewRouter()
+
+	r.Get("/exchanges", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("currency"))
+		if err != nil {
+			return
+		}
+	})
+
+	r.Get("/exchanges/{idSrcCurrency}", func(w http.ResponseWriter, r *http.Request) {
+		idSrcCurr := chi.URLParam(r, "idSrcCurrency")
+		log.Println("idSrcCurrency:", idSrcCurr)
+		_, err := fmt.Fprintf(w, "idSrcCurrency: %s\n", idSrcCurr)
+		if err != nil {
+			return
+		}
+	})
+
+	r.Get("/exchanges/{idSrcCurrency}/{idDstCurrency}", func(w http.ResponseWriter, r *http.Request) {
+		idSrcCurr := chi.URLParam(r, "idSrcCurrency")
+		idDstCurr := chi.URLParam(r, "idDstCurrency")
+		log.Println("idSrcCurrency:", idSrcCurr)
+		log.Println("idDstCurrency:", idDstCurr)
+		_, err := fmt.Fprintf(w, "idSrcCurrency: %s, idDstCurrency: %s\n", idSrcCurr, idDstCurr)
+		if err != nil {
+			return
+		}
+	})
+
+	// Start HTTP server
+	err = http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Panicf("error starting server: %v", err)
+	}
 }
